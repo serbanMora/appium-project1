05434b007ce9fd0de1c8f4a9a8beed7107bb0007
Lynx User <serban_mora@yahoo.com>
2024-12-02 17:32:15 +0200

    updates

--- src/main/java/GeneralStore/Config/BaseTestUtils.java
+++ src/main/java/GeneralStore/Config/BaseTestUtils.java
@@ -1,6 +1,9 @@
 package GeneralStore.Config;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
@@ -9,9 +12,14 @@ public class BaseTestUtils {
 	
 	public AppiumDriverLocalService service;
 
-	public AppiumDriverLocalService startAppiumServer(String ipAddress, int port) {
+	public AppiumDriverLocalService startAppiumServer(String ipAddress, int port) throws IOException {
+		Properties prop = new Properties();
+		FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "/src/main/java/GeneralStore/Config/data.properties");
+		prop.load(fis);
+		String mainjsPath = prop.getProperty("mainjsPath");
+		
 		service = new AppiumServiceBuilder()
-				.withAppiumJS(new File("C:\\Users\\Lynx User\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js"))
+				.withAppiumJS(new File(mainjsPath))
 				.withIPAddress(ipAddress).usingPort(port).build();
 		service.start();
 		return service;
